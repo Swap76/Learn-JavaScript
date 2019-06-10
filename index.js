a9702daffe675b2fd87fe9ca8546861766a96bad
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-06-10 12:25:42 +0530

    This is first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let into = "My collge is SIESGST. Our seniors and aluminies are best in Mumabi";
+let fullName = "My name is Swapnil Satish Shinde";
+let branch = "I belong to Computer Science branch";
+let bio = `${into} ${fullName} ${branch}`;
+console.log(bio);
---
+++ variables.js
@@ -0,0 +1,8 @@
+// There is only 3 types of variables in javascript
+
+let a = "Swapnil"; // New type introdued in ES6. Value of let can change any time.
+
+
+const pi = 3.14;// New type introdued in ES6. Value of pi now cannot be changed as this is defined as const.
+
+var b = 26;// This is Depricated as this creates many probles in future.
