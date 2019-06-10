5d83c9b7b9eef7b4e9b57d1ee2f2211d79c97f31
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-06-10 15:52:02 +0530

    Completed Classes.js

--- classes.js
+++ classes.js
@@ -4,13 +4,24 @@ class Student {
     this.name = name; // This initializes the local variable as name passed in argument
     this.age = age; // This initializes the local variable as age passed in argument
   }
-  // function printClass (){
-  //   console.log(`${this.name} ${this.age}`);
-  // }
 }
 
-Swapnil = new Student("Swapnil", 19); // This way we can create new objects with arguments
+let Swapnil = new Student("Swapnil", 19); // This way we can create new objects with arguments
 
 console.log(Swapnil.name);// Output Swapnil
 
 console.log(Swapnil.age);// Output 19
+
+class StudentInfo {
+  // college = "SIES"; // This is allowed above ES7, ES8
+  constructor (name){ // name and age are arguments given to object at the time of creation of object
+    this.name = name; // This initializes the local variable as name passed in argument
+    this.college = "SIES"; // We want the College to be same for all stundents that's why it is decleared outside of constructor
+  }
+  getNameAndCollege (){ // This is a methon in Stdent
+    console.log(`${this.name} ${this.college}`);
+  }
+}
+
+let SwapnilInfo = new StudentInfo("Swapnil Bio");
+SwapnilInfo.getNameAndCollege();
