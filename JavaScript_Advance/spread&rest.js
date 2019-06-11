9419fd276cfc7a7f734eee498ad5f34a8c37b26e
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-06-11 14:05:29 +0530

    Added timerFunction.js and completed spread&rest.js

---
+++ JavaScript_Advance/spread&rest.js
@@ -0,0 +1,52 @@
+let marks = [1,2,3,4,5,6,7,8,9]; // Array Declearation
+
+// If we want 1 to 3 as diffrent variables and 4 to 9 as full array then in such cases we use "rest" operator
+
+let [first, second, third, ...remaining] = marks;
+
+console.log(first, second, third); // Output 1 2 3
+
+console.log(remaining); // Output [ 4, 5, 6, 7, 8, 9 ]
+
+let a = {
+  name : "Swapnil",
+  branch : "Comps",
+  age : 19,
+  college : "SIES"
+};
+
+let {name ,age, ...unwanted} = a; // Here in unwanted all key and values expect the name and age will come this is diffrent than arrays
+
+console.log(unwanted);// Output { branch: 'Comps', college: 'SIES' }
+
+
+// "spread" operator can also be used for merging the arrays
+
+let array1 = [1,2,3,4,5,6];
+
+let array2 = [6,7,8,9,10];
+
+let combinedArray  = [...array1, ...array2]; // Here "..." are considerd as spread operation we are spreading the two arrays into one
+
+console.log(combinedArray); 
+/* Output 
+[
+  1,  2,  3,
+  4,  5,  6,
+  6,  7,  8,
+  9, 10
+] */
+
+let obj1 = { // First Object
+  name : "Swapnil",
+  branch : "Comps"
+};
+
+let obj2 = { // Second Object
+  age : 19,
+  college : "SIES",
+}
+
+let finalObj = {...obj1, ...obj2}; // Using Spread operator we are combining two objects into one
+
+console.log(finalObj); // Output { name: 'Swapnil', branch: 'Comps', age: 19, college: 'SIES' }
---
+++ JavaScript_Advance/timerFunctions.js
@@ -0,0 +1,27 @@
+// setTimeout setInterval in both functions the time is specifed in milisecond
+
+// setTimeout This runs a code after given time starting form calling this method in program.
+
+setTimeout( () => {
+  console.log("SetTimeout is Called")
+}, 5000); // This will print SetTimeout is Called after 5sec after calling it
+
+// For stoping this we can use ClearTimeout
+
+// setInterval This runs a code after specific interval of time till we stop the timer.
+
+starting = setInterval( () => {
+  console.log("SetInterval is Called")
+}, 3000); // This will print setInterval is Called after every 3sec 
+
+// We can stop the interval and timeout using clearTimeout and clearInterval
+setTimeout(()=>{
+  clearInterval(starting);
+}, 7000);
+
+// Output of this whole program is 
+/*
+SetInterval is Called
+SetTimeout is Called
+SetInterval is Called
+*/
