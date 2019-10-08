5034120f52b555a0d499b6547ade93f9a1a25fdc
RahulBudhraj <rahulbudhraj@outlook.com>
2019-10-08 17:04:15 +0530

    Comments has been added and spaces have been removed

--- JavaScript_Advance/nasaimgHttps.js
+++ JavaScript_Advance/nasaimgHttps.js
@@ -1,51 +1,37 @@
-var https = require("https");
-var fs = require("fs");
+var https = require("https"); // requiring https module
+var fs = require("fs");      //  requiring fs module
 var Stream = require("stream").Transform;
 const options = {
-    hostname: "api.nasa.gov",
+    hostname: "api.nasa.gov",  // nasa api 
     port: 443, // tcp port
-    path: "/planetary/apod?api_key=DEMO_KEY&date=2019-08-08",
+    path: "/planetary/apod?api_key=DEMO_KEY&date=2019-08-08", // path from which image is extracted 
     method: 'GET'
 };
 var wholedata = "";
 var request = https.request(options, function (res) {
-
-
     res.on("data", function (chunk) {
-        wholedata += chunk;
+        wholedata += chunk; // we get data in chunks or packets so we are adding it to get wholedata
 
     });
-
-
     res.on("end", function () {
 
-        var JSONobj = JSON.parse(wholedata);
+        var JSONobj = JSON.parse(wholedata); // parsing data into object form
 
         var path = JSONobj["url"];
         var img = https.request(path, function (res) {
-            var data = new Stream();
+            var data = new Stream(); // creating new stream
 
             res.on("data", function (chunk) {
                 data.push(chunk);
-
-
             });
 
             res.on("end", function () {
-                fs.writeFileSync("file.jpg", data.read())
+                fs.writeFileSync("file.jpg", data.read())  // saving image in file.jpg file
 
             });
-
         });
         img.end();
-
-
-
-
-
     });
 
-
-
 })
-request.end();
+request.end(); // ending request
