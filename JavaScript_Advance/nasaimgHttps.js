996f1baa1362fc45ea7738bd848a31bffd2e0c15
RahulBudhraj <rahulbudhraj@outlook.com>
2019-10-09 16:11:39 +0530

    Applied ES6 syntax

--- JavaScript_Advance/nasaimgHttps.js
+++ JavaScript_Advance/nasaimgHttps.js
@@ -1,22 +1,22 @@
-var https = require("https"); // requiring https module
-var fs = require("fs");      //  requiring fs module
-var Stream = require("stream").Transform;
+const https = require("https"); // requiring https module
+const fs = require("fs");      //  requiring fs module
+const Stream = require("stream").Transform;
 const options = {
     hostname: "api.nasa.gov",  // nasa api 
     port: 443, // tcp port
     path: "/planetary/apod?api_key=DEMO_KEY&date=2019-08-08", // path from which image is extracted 
     method: 'GET'
 };
-var wholedata = "";
-var request = https.request(options, function (res) {
+const wholedata = "";
+const request = https.request(options, function (res) {
     res.on("data", function (chunk) {
         wholedata += chunk; // we get data in chunks or packets so we are adding it to get wholedata
     });
     res.on("end", function () {
-        var JSONobj = JSON.parse(wholedata); // parsing data into object form
-        var path = JSONobj["url"];
-        var img = https.request(path, function (res) {
-            var data = new Stream(); // creating new stream
+        const JSONobj = JSON.parse(wholedata); // parsing data into object form
+        const path = JSONobj["url"];
+        const img = https.request(path, function (res) {
+            const data = new Stream(); // creating new stream
             res.on("data", function (chunk) {
                 data.push(chunk);
             });
