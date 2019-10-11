c66296a5c992410988cfa2efedf6d25fa8ffcc03
mubashir ali siddiqui <mubashir22ali@gmail.com>
2019-10-11 16:07:05 +0500

    arr.some javascript example

---
+++ JavaScript_Advance/someMethod.js
@@ -0,0 +1,23 @@
+
+
+//Array some() Method
+//The some() method executes the function once for each element present in the array:
+//If it finds an array element where the function returns a true value, 
+// some() returns true (and does not check the remaining values)
+
+
+
+let arr = ['name','test name', 'testtwo','ship'];
+
+// syntax
+
+let found = arr.some((element) => {
+  console.log(element) // nam ,test name,  true;  stop iterating
+  // if the condition matched it doesn't check for the whole array
+  // benifical where you want to check if a proprty in a whole array exist
+   return element.includes('test')
+})
+console.log(found) // true
+
+
+// it doesnot change the original array
