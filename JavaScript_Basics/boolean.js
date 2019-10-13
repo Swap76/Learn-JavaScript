ff8b2df6baccf12cb5fb253629496cb352fba235
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-10-13 19:10:46 +0530

    combined two page-redirect files together

--- JavaScript_Basics/boolean.js
+++ JavaScript_Basics/boolean.js
@@ -1,4 +1,6 @@
 // JavaScript Boolean data type can store one of two values, true or false. ... e.g.
 const YES = new Boolean(true);
+
 // JavaScript treats an empty string (""), 0, undefined and null as false.
+
 // Everything else is true.
