bcd62de24fb9fc31abae13913367eb2a58500ca3
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-10-13 19:27:13 +0530

    Added file for FS module to read

--- JavaScript_Advance/fsModule.js
+++ JavaScript_Advance/fsModule.js
@@ -8,7 +8,7 @@
 const fs = require("fs");
 
 // Here we created a read stream will give output as a object into string
-const content = fs.createReadStream("./Order.txt");
+const content = fs.createReadStream("./resources/file/order.txt");
 
 /* If we don't use the encoding as utf8 then it will print data in the form of buffer only
 <Buffer 61 72 72 6f 77 46 75 6e 63 74 69 6f 6e 0a 64 65 73 74 72 75 63 74 75 72 69 6e 67 0a 73 70 72 65 61 64 20 61 6e 64 20 72 65 73 74 0a 74 69 6d 65 72 46 ... 35 more bytes>
