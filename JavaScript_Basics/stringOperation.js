4b54f93f0eaf5ddde376ae046e4575c2ce35f28c
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-10-13 19:15:38 +0530

    String Opration updated

--- JavaScript_Basics/stringOperation.js
+++ JavaScript_Basics/stringOperation.js
@@ -1,7 +1,15 @@
-const into = "My collge is SIESGST. Our seniors and aluminies are best in Mumabi";
+const into = "My collge is SIESGST. Our seniors and aluminies are best in Mumbai";
+
 const fullName = "My name is Swapnil Satish Shinde";
+
 const branch = "I belong to Computer Science branch";
+
 const oldBio = into + " " + fullName + " " + branch;
+
 const bio = `${into} ${fullName} ${branch}`; // New way of concatenation
-console.log(oldBio);
-console.log(bio);
+
+console.log(oldBio); // My collge is SIESGST. Our seniors and aluminies are best in Mumbai My name is Swapnil Satish Shinde I belong to Computer Science branch
+
+console.log(bio); // My collge is SIESGST. Our seniors and aluminies are best in Mumbai My name is Swapnil Satish Shinde I belong to Computer Science branch
+
+// Output by both the methods is same but string interpolation is more beautifull as we only have to right 1 line for all we want
