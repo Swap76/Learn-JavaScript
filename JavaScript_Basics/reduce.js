6660d69c9917bb5bceb27b789c32b673144e10b1
Avijit Das <adavijit@yahoo.com>
2019-10-14 15:33:32 +0530

    higher order functions

---
+++ JavaScript_Basics/reduce.js
@@ -0,0 +1,15 @@
+//JavaScript | Array reduce() Method
+/*
+The array reduce() method in JavaScript is used to reduce 
+the array to a single value and executes a provided function
+for each value of the array (from left-to-right) and the return
+value of the function is stored in an accumulator.
+*/
+var pokemon = ["squirtle", "charmander", "bulbasaur"];
+
+var pokeLength =
+  pokemon.reduce(function(previous, current) {
+  	return previous + current.length;
+  }, 0);
+
+// Outputs 27
