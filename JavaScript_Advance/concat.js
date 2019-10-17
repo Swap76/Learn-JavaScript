3ed6b0450778b26df36d6596e2cdf31d211ce8b1
CHARITH1995 <prasanna.charith32@gmail.com>
2019-10-17 14:25:29 +0530

    concat function

--- JavaScript_Advance/concat.js
+++ JavaScript_Advance/concat.js
@@ -1,15 +1,8 @@
 
-/*
-The concat() method is used to join two or more arrays. Concat does not alter the original arrays, 
-it returns a copy of the same elements combined from the original arrays.
-*/
-//Example 1:
-console.log('It'.concat(' is',' a',' great',' day.')); 
-//Output : It is a great day.
-
-//Example 2:
-var str1 = new String( "hello" );
-var str2 = new String( "world" );
-var str3 = str1.concat( str2 );      
-document.write("concat output :" + str3); 
-//Output : Concatenated String :This is string oneThis is string two.
+function myFunction() {
+  var str1 = "Hello ";
+  var str2 = "world!";
+  var str3 = " Have a nice day!";
+  var res = str1.concat(str2, str3);
+  document.write("concat output :" + res);
+}
