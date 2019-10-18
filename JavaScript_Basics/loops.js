4da0b35e4e87ca6290f3db29d8dae9a4faf36c51
Zeel Modi <zeel91297@gmail.com>
2019-10-18 18:11:10 +0530

    while and do...while loops added

---
+++ JavaScript_Basics/loops.js
@@ -0,0 +1,17 @@
+//do...while loop
+var text = "";
+var i = 0;
+do {
+    text = "i = " + i;
+    console.log(text);
+    i++;
+} while (i < 5);
+
+console.log("\n");
+//while loop
+i = 0;
+while (i < 5) {
+    text = "i = " + i;
+    console.log(text);
+    i++;
+}
