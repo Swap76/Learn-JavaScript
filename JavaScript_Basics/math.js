980f5f582c7e43f1495059e6ab7a1e88e710741d
sb <snehabodke.sb@gmail.com>
2019-10-19 15:14:58 +0530

    added math.js and math.md files

--- JavaScript_Basics/math.js
+++ JavaScript_Basics/math.js
@@ -2,6 +2,8 @@
 
 
 //Math.round(number) rounds off the value of number to nearest integer.
+//when a string input is provided it says "NaN"
+//It can take number in string format as well.
 let num1 = 24.45;
 let num2 = 24.5;
 let num3 = 24.95;
@@ -20,17 +22,32 @@ num5 = Math.round(num5);
 console.log(num5);
 
 //Math.pow(x,y) returns base to the exponent power, that is, base^exponent.
+//when a string input is provided it says "NaN"
+//It can take number in string format as well.
 let x = 2;
 let y = 3;
+let p = "2";
 let z = Math.pow(x,y);
+let q = Math.pow(p,y);
 console.log(z);
+console.log(q);
 
 //Math.sqrt(x) returns square root of the given number.
+//when a string input is provided it says "NaN"
+//It can take number in string format as well.
 let x = 4;
+let y = "16";
+let result1 = Math.sqrt(y);
 let result = Math.sqrt(x);
 console.log(result);
+console.log(result1);
 
 //Math.abs(x) returns absolute value of a number.
+//when a string input is provided it says "NaN"
+//It can take number in string format as well.
 let x = -24;
+let y = "-24";
+let result1 = Math.abs(y);
 let result = Math.abs(x);
 console.log(result);
+console.log(result1);
--- docs/JavaScript_Basics/math.md
+++ docs/JavaScript_Basics/math.md
@@ -1,4 +1,6 @@
-Math is a built-in object that has properties and methods for mathematical constants and functions. Not a function object.
+Math is a built-in object that has properties and methods for mathematical constants and functions.
+All the Math functions take input as number. If a number is provided in string format, It is accepted as well,
+If a string input is provided like Math.abs("hello") It will output "NaN" ie not a number.
 
 Math.round(number) rounds off the value of number to nearest integer
 
