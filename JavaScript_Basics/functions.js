4f7def1fb0c3f4e1ddb2fd91ced15167f858ec96
phyyyl <phyyyl@eclipso.de>
2019-10-24 19:36:10 +0200

    Explained basic usage of functions
    * What is a function?
    * Common declaration
    * Basic usage of "return" keyword
    * Basic usage of parameters

--- JavaScript_Basics/functions.js
+++ JavaScript_Basics/functions.js
@@ -1,40 +1,62 @@
-function helpGST () { // This way you can write functions in JS
-  // There is no requirement of specifying the return type of function
-  console.log("Hii GST");
-}
-// Function is written to do a specific task many types in program.
-// This increases the maintainability and decreases the repetition of same code.
-helpGST(); // For executing we have to call that function
+// Functions in Javascript
+// Functions are used to structure and generalize the code. It will become more flexible and you will have a better overview.
 
-function Add (a, b) { // Here We have taken 2 Arguments a and b
-  const c = a + b;
-  return c; // This returns the result
-}
+/* What is a function?
+A Function is a type of varable which essentially contains code you can run.
+There are different ways to write code into a variable (declare a function).
+*/
 
-console.log(Add(10, 5)); // This Calls the function
-// We have to directly pass the arguments in function call
-
-// Functions can have a default parameter value.
-// Default value will be used if the argument is not defined.
-function subtract(a, b = 1) {
-  const c = a - b;
-  return c;
+function foo () { // Puts codes inside if the curly brackets into the variable "foo".
+  console.log("Hello World")
 }
 
-console.log(subtract(5)); // Output 4.
-console.log(subtract(5, 3)); // Output 2.
-console.log(subtract(5, null)); // Output 5, because null is a valid value.
+// Shows that "foo" is a variable with the type function
+console.log(typeof foo) // "function"
+console.log(typeof 1) // "number"
+console.log(typeof "hello") // "string"
 
-// Functions which don't have name are called Anonymous this should be assigned to a particular variable
-const helpFast = function () {
-  return ("Fast Fast");
-};
-console.log(helpFast());
-// Output Fast Fast
+/* How to call a function?
+The function is defined. Now we want to execute the code inside the Function
+*/
 
-const helpVeryFast = () => {
-  return ("Very Fast");
-};
+// The most common way to execute a function by appending the variable name with brackets
+foo() // "Hello World"
 
-console.log(helpVeryFast());
-// Output Very Fast
+/* Functions with output
+Printing text is acually the wrong format of getting informations out of variables.
+To get usable informations back we can use the return keyword.
+*/
+
+// Nothing is returned -> x is empty
+x = foo() // "Hello World" and x: undefined
+
+
+function foo () { // get a value from the inside of the function to te outside.
+  text = "Hello World"
+  return text
+}
+
+// text is returned, nothing prited into the console.
+x = foo() // x: "Hello World"
+
+// You can see that variables from inside the function are not accessible anymore
+console.log(text) // undefined
+// the string from the "text" variable was retuned an is now "x"
+console.log(x) // "Hello World"
+
+/* Functions with input
+Input for functions is called parameters.
+When you define the function you can also define what input variables the function can have.
+That is the purpose of the "()". "()" means no input both wen you define and call a function.
+*/
+
+// define with parameters
+function add(a, b) { // in the "()" you define what variable names the inputs get. Multiple parameters are septerated by a ","
+  c = a + b // add the value of the two variables
+  return c
+}
+
+// call with parameters
+// Depending on the sequence, the values separated by a "," are assigned to the parameter variables.
+// a = 5; b = 8;
+result = add(5, 8) // result: 13
