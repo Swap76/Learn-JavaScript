2bdb841a025345b80a80b6308457de9136a71104
Henrique Cunha <henriquecunhawd@gmail.com>
2019-10-28 08:45:10 -0300

    Update string_operation.js

--- JavaScript_Basics/string_operation.js
+++ JavaScript_Basics/string_operation.js
@@ -21,3 +21,8 @@ console.log(bio); // My college is SIESGST. Our seniors and alumnus are best in 
 String.prototype.capitalize = (string) => string.slice(0, 1).toUpperCase() + string.slice(1)
 console.log("a casual lower case string")
 // Prints out: "A casual lower case string"
+
+
+/** EXERCISE **/
+// Add a function to the string prototype that counts the occurrences of a substring
+// 'cheese and Bananas, and more cheese'.count('cheese') -----> 2
