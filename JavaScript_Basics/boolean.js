0605447cb52144e3eddc59ebff4ed5a957c19664
ferdiaz93 <fernandodiaz1209@gmail.com>
2019-10-30 22:20:05 -0300

    more examples added

--- JavaScript_Basics/boolean.js
+++ JavaScript_Basics/boolean.js
@@ -1,6 +1,17 @@
 // JavaScript Boolean data type can store one of two values, true or false. ... e.g.
 const YES = new Boolean(true);
 
+// It can be declare to this way too
+var negativeVariable = false;
+
+//You can use the operator '!' to invert the value of the boolean
+console.log(!sunnyDay) // Output: {true}
+
+//also you can use the operator '||' or '&&' to envolv the result of two booleans in one value
+var positiveVariable = true;
+console.log(negativeVariable || positiveVariable) //Output: {true}
+console.log(negativeVariable && positiveVariable) //Output: {false}
+
 // JavaScript treats an empty string (""), 0, undefined and null as false.
 
 // Everything else is true.
